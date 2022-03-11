de1ad0b8a4e304b001975b7023d6609896f21eee
natiabebaw <natiabebaw35@gmail.com>
2022-03-11 06:58:08 -0800

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = '1'; number <= '9'; number++)
+	{
+		putchar(number);
+	}
+	printf("\n");
+
+
+	return (0);
+}
