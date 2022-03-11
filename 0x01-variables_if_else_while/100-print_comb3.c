c459eac30856349464a2e7e30438e9ca94bb2f23
natiabebaw <natiabebaw35@gmail.com>
2022-03-11 08:44:50 -0800

    bug fixed Q6

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = '0'; num1 <= '9'; num1++)
+	for (num1 = '0'; num1 < '9'; num1++)
 	{
 		for (num2 = num1 + 1; num2 <= '9'; num2++)
 		{
@@ -17,7 +17,8 @@ int main(void)
 			{
 				putchar(num1);
 				putchar(num2);
-				if (num1 != '8' && num2 != '9'){
+				if (num1 == '8' && num2 == '9')
+				{
 					continue;
 				}
 			}
