61f393d3a3a30a4d8b873222b44bd98271d91960
natiabebaw <natiabebaw35@gmail.com>
2022-03-16 13:09:44 +0300

    0._putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,18 @@
-#include <stdio.h>
 #include "main.h"
+#include <string.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0..
+ * the program prints "_putchar" followed by a new line.
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char word[] = "_putchar";
+	int i = 0;
 
+	for (i; i < strlen(word); i++)
+	{
+		_putchar(word[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
